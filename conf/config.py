ab74b9b083afc77f0d2142473393a96daf37652f
Thomas Uhde <thomas.uhde@acidix.net>
2013-09-09 17:42:20 +0200

    Refactoring

---
+++ conf/config.py
@@ -0,0 +1,20 @@
+__config__ = {
+            "racktables":
+                {
+                    "apiurl": "http://racktables/api.php",
+                    "username": "admin",
+                    "password": "secret"
+                },
+            "vsphere":
+                {
+                    "server": "vsphere",
+                    "username": "root",
+                    "password": "secret"
+                },
+            "general":
+                {
+                    "objtype_id": "1504",
+                    "linkparent": "25",
+                    "dnssuffix": ".mycompany.lan"
+                }
+}
